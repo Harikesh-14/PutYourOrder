438a48c84d437bbd8b63f47e90fab074e635566f
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-06-02 14:53:18 +0530

    Dashboard done

--- client/src/container/AdminDashboard.tsx
+++ client/src/container/AdminDashboard.tsx
@@ -1,20 +1,151 @@
+import { BiSolidPencil } from "react-icons/bi"
+import { CiUser } from "react-icons/ci"
+import { Link } from "react-router-dom"
+
 function AdminDashboard() {
   return (
-    <div
-      className="md:ml-[20rem] p-5 pt-6 bg-gray-100"
-    >
-      <h1 className="text-2xl font-semibold text-gray-800">Admin Dashboard</h1>
-      <div className="my-10 bg-white">
-        <p className="text-gray-600 mt-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          dolorum, voluptatum, iusto, quae quos quidem voluptatem corrupti
-          asperiores possimus quas autem. Quisquam dolorum, voluptatum, iusto,
-          quae quos quidem voluptatem corrupti asperiores possimus quas autem.
-          Quisquam dolorum, voluptatum, iusto, quae quos quidem voluptatem
-          corrupti asperiores possimus quas autem. Quisquam dolorum, voluptatum,
-          iusto, quae quos quidem voluptatem corrupti asperiores possimus quas
-          autem.
-        </p>
+    <div className="md:ml-[20rem] p-10 bg-gray-100">
+      <h1 className="text-2xl md:text-3xl font-semibold text-gray-800">Admin Dashboard</h1>
+      <div className="my-7 bg-white border-b border-gray-300">
+        <div className="py-4">
+          <h2 className="flex items-center gap-2 text-lg md:text-xl font-semibold text-gray-800 px-3">
+            Profile
+            <CiUser />
+          </h2>
+        </div>
+        <div className="flex justify-between items-center p-3 md:p-5 md:px-10">
+          <label
+            className="w-[74%] md:w-[15%] text-gray-800 font-semibold text-md md:text-lg"
+          >First Name</label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded p-2"
+            value={"Harikesh Ranjan"}
+            disabled
+          />
+          <Link
+            to={'#'}
+            className="bg-blue-500 text-white font-semibold text-sm md:text-lg ml-2 p-3 rounded hover:bg-blue-600 transition duration-200 ease-in-out"
+          >
+            <BiSolidPencil />
+          </Link>
+        </div>
+
+        <div className="flex justify-between items-center p-3 md:p-5 md:px-10">
+          <label
+            className="w-[74%] md:w-[15%] text-gray-800 font-semibold text-md md:text-lg"
+          >Last Name</label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded p-2"
+            value={"Sinha"}
+            disabled
+          />
+          <Link
+            to={'#'}
+            className="bg-blue-500 text-white font-semibold text-sm md:text-lg ml-2 p-3 rounded hover:bg-blue-600 transition duration-200 ease-in-out"
+          >
+            <BiSolidPencil />
+          </Link>
+        </div>
+
+        <div className="flex justify-between items-center p-3 md:p-5 md:px-10">
+          <label
+            className="w-[74%] md:w-[15%] text-gray-800 font-semibold text-md md:text-lg"
+          >Gender</label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded p-2"
+            value={"Male"}
+            disabled
+          />
+          <Link
+            to={'#'}
+            className="bg-blue-500 text-white font-semibold text-sm md:text-lg ml-2 p-3 rounded hover:bg-blue-600 transition duration-200 ease-in-out"
+          >
+            <BiSolidPencil />
+          </Link>
+        </div>
+
+        <div className="flex justify-between items-center p-3 md:p-5 md:px-10">
+          <label
+            className="w-[74%] md:w-[15%] text-gray-800 font-semibold text-md md:text-lg"
+          >Email</label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded p-2"
+            value={"ranjansinhaharikesh@gmail.com"}
+            disabled
+          />
+          <Link
+            to={'#'}
+            className="bg-blue-500 text-white font-semibold text-sm md:text-lg ml-2 p-3 rounded hover:bg-blue-600 transition duration-200 ease-in-out"
+          >
+            <BiSolidPencil />
+          </Link>
+        </div>
+
+        <div className="flex justify-between items-center p-3 md:p-5 md:px-10">
+          <label
+            className="w-[74%] md:w-[15%] text-gray-800 font-semibold text-md md:text-lg"
+          >Phone Number</label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded p-2"
+            value={"9319958749"}
+            disabled
+          />
+          <Link
+            to={'#'}
+            className="bg-blue-500 text-white font-semibold text-sm md:text-lg ml-2 p-3 rounded hover:bg-blue-600 transition duration-200 ease-in-out"
+          >
+            <BiSolidPencil />
+          </Link>
+        </div>
+      </div>
+
+      <div className="my-7 bg-white border-b border-gray-300">
+        <div className="py-4">
+          <h2 className="flex items-center gap-2 text-lg md:text-xl font-semibold text-gray-800 px-3">
+            Change Password
+            <CiUser />
+          </h2>
+        </div>
+        <div className="flex justify-between items-center p-3 md:p-5 md:px-10">
+          <label
+            className="w-[74%] md:w-[15%] text-gray-800 font-semibold text-md md:text-lg"
+          >Old Password</label>
+          <input
+            type="password"
+            className="w-full border border-gray-300 rounded p-2"
+          />
+        </div>
+
+        <div className="flex justify-between items-center p-3 md:p-5 md:px-10">
+          <label
+            className="w-[74%] md:w-[15%] text-gray-800 font-semibold text-md md:text-lg"
+          >New Password</label>
+          <input
+            type="password"
+            className="w-full border border-gray-300 rounded p-2"
+          />
+        </div>
+
+        <div className="flex justify-between items-center p-3 md:p-5 md:px-10">
+          <label
+            className="w-[74%] md:w-[15%] text-gray-800 font-semibold text-md md:text-lg"
+          >Confirm Password</label>
+          <input
+            type="password"
+            className="w-full border border-gray-300 rounded p-2"
+          />
+        </div>
+
+        <div className="flex justify-center items-center p-3 md:p-5 md:px-10">
+          <button
+            className="bg-blue-500 text-white font-semibold text-md md:text-lg p-3 rounded hover:bg-blue-600 transition duration-200 ease-in-out"
+          >Change Password</button>
+        </div>
       </div>
     </div>
   )
