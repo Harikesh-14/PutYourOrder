1f4be27c46bca903f1ae736fda55512289728816
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-06-03 17:26:20 +0530

    Created the update api for the admin

--- api/src/Routes/admin.ts
+++ api/src/Routes/admin.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from "express";
 import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import adminModel from "../../models/admin";
@@ -11,6 +11,15 @@ const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET_KEY as string;
 
 router.use(cookieParser());
+
+interface AdminJwtPayload extends JwtPayload {
+  id: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  email: string;
+  phoneNumber: string;
+}
 
 router.post("/register", async (req: Request, res: Response) => {
   const { firstName, lastName, gender, email, phoneNumber, password } = req.body;
@@ -93,6 +102,45 @@ router.get("/profile", (req: Request, res: Response) => {
 
 router.post("/logout", async (req: Request, res: Response) => {
   res.clearCookie("token").json({ message: "Logged out" });
+});
+
+router.put("/update", async (req: Request, res: Response) => {
+  const { token } = req.cookies;
+  const { firstName, lastName, gender, email, phoneNumber } = req.body;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  jwt.verify(token, secret, {}, async (err, decoded) => {
+    if (err) {
+      console.error('JWT verification error:', err);
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    if (typeof decoded !== 'string') {
+      const updateFields: any = {};
+      if (firstName) updateFields.firstName = firstName;
+      if (lastName) updateFields.lastName = lastName;
+      if (gender) updateFields.gender = gender;
+      if (email) updateFields.email = email;
+      if (phoneNumber) updateFields.phoneNumber = phoneNumber;
+
+      try {
+        const updatedAdmin = await adminModel.findByIdAndUpdate(
+          (decoded as AdminJwtPayload).id,
+          updateFields,
+          { new: true }
+        );
+
+        res.json(updatedAdmin);
+      } catch (err) {
+        res.status(500).json({ message: "Internal server error" });
+      }
+    } else {
+      res.status(401).json({ error: 'Unauthorized' });
+    }
+  });
 });
 
 export default router;
