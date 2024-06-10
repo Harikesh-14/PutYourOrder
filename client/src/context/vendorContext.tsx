d4b3a48f1652100a43212f1d961bbb84493153cc
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-06-10 18:30:29 +0530

    Created Vendor Context API

--- client/src/context/vendorContext.tsx
+++ client/src/context/vendorContext.tsx
@@ -1,7 +1,48 @@
-function vendorContext() {
+import React, { createContext, useState } from "react"
+
+type VendorType = {
+  id: string
+  firstName: string
+  lastName: string
+  gender: string
+  email: string
+  phoneNumber: number
+  message?: string
+}
+
+type VendorContextType = {
+  vendorLoggedIn: VendorType
+  setVendorLoggedIn: React.Dispatch<React.SetStateAction<VendorType>>
+  isVendorLoggedIn: boolean
+  setIsVendorLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const VendorContext = createContext<VendorContextType | null>(null)
+
+function VendorContextProvider({ children }: { children: React.ReactNode}) {
+  const [vendorLoggedIn, setVendorLoggedIn] = useState<VendorType>({
+    id: "",
+    firstName: "",
+    lastName: "",
+    gender: "",
+    email: "",
+    phoneNumber: 0,
+    message: "",
+  })
+  const [isVendorLoggedIn, setIsVendorLoggedIn] = useState<boolean>(false)
+
+  const value = {
+    vendorLoggedIn,
+    setVendorLoggedIn,
+    isVendorLoggedIn,
+    setIsVendorLoggedIn,
+  }
+
   return (
-    <div>vendorContext</div>
+    <VendorContext.Provider value={value}>
+      {children}
+    </VendorContext.Provider>
   )
 }
 
-export default vendorContext
+export default VendorContextProvider
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AdminContextProvider from './context/adminContext.tsx'
+import VendorContextProvider from './context/vendorContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AdminContextProvider>
-        <App />
+        <VendorContextProvider>
+          <App />
+        </VendorContextProvider>
       </AdminContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
